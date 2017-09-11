556b5e43c7673015e508ae464dbadc90479fbe0d
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2017-09-11 17:31:40 -0400

    Updated kprintf with reading variable args and fixed minor print bugs.

--- sys/kprintf.c
+++ sys/kprintf.c
@@ -1,7 +1,9 @@
 #include <sys/kprintf.h>
+#include <sys/stdarg.h>
 
 char* outputWriteAddress = (char*)0xb8000;
 long totalCharsPrinted = 0;
+va_list args;
 typedef enum format_type {
     STRING,
     INT,
@@ -16,105 +18,107 @@ void printHex(long x);
 void printLong(long x);
 void printVoid(void* v);
 
-void kprintf(const char *fmt, ...)
-{
-    const char* c = &fmt[0];
+void kprintf(const char *fmt, ...) {
+    va_start(args, fmt);
+    const char* c = fmt;
     int specialCharCount = 0;
-    while (*c!='\0') {
+    int argNumber = 0;
+    while (*c != '\0') {
         if (*c == '%') {
             //special character case
-            switch (*(c+1)) {
+            switch (*(c + 1)) {
                 case 's':
+                    printSpecial(argNumber++, STRING);
                     break;
                 case 'd':
+                    printSpecial(argNumber++, INT);
                     break;
                 case 'x':
+                    printSpecial(argNumber++, HEX);
                     break;
                 case 'p':
+                    printSpecial(argNumber++, VOID);
                     break;
                 default:
                     break;
             }
             specialCharCount++;
-            c+=2;
+            c += 2;
         } else {
             printChar(*c);
             c++;
         }
     }
-    printString("Hello");
+    va_end(args);
 }
 
 void printSpecial(int argNumber, format_type ft) {
-    switch (argNumber) {
-        case 0:
-            //extract RDI;
-            break;
-        case 1:
-//            extract RSI:
-            break;
-        case 2:
-//            extract RDX;
-            break;
-        case 3:
-//            extract RCX;
-            break;
-        case 4:
-//            extract R8:
-            break;
-        case 5:
-//            extract R9:
-            break;
-
-        default:
-            break;
+    if (ft == STRING) {
+        printString(va_arg(args, char*));
+    }
+    else if (ft == INT) {
+        printLong(va_arg(args, int));
+    }
+    else if (ft == HEX) {
+        printHex(va_arg(args, long));
+    }
+    else if (ft == VOID) {
+        printVoid(va_arg(args, void*));
     }
 }
 
-
 void printChar(char c) {
-    (*outputWriteAddress) = c;
+    *outputWriteAddress = c;
     outputWriteAddress += 2;
     totalCharsPrinted++;
 }
 
 
 void printString(char *c) {
-    while (c!='\0') {
+    while (*c != '\0') {
         printChar(*(c++));
     }
 }
 
 void printLong(long x) {
     char buf[66];
-    int i = 66;
+    int i = 65;
     buf[i--] = '\0';
     long numCopy = x;
     while(numCopy) {
-        buf[i--] = numCopy % 10;
+        buf[i--] = 48 + numCopy % 10;
         numCopy /= 10;
     }
     if (x < 0) {
         buf[i] = '-';
+        printString((char*) (buf) + i);
     }
-    printString(buf + i);
+    else {
+        printString((char*) (buf) + i + 1);
+    }
 }
 
 void printHex(long x) {
     char buf[19];
-    int i = 19;
+    int i = 18;
     buf[i--] = '\0';
     long numCopy = x;
     while(numCopy) {
-        buf[i--] = numCopy % 16;
+        int remain = numCopy % 16;
+        if (remain < 10) {
+            buf[i--] = 48 + remain;
+        }
+        else {
+            buf[i--] = 65 + (remain - 10);
+        }
         numCopy = numCopy >> 4;
     }
     buf[i--] = 'x';
     buf[i] = '0';
-    printString(buf + i);
+    printString((char*)(buf) + i);
 }
 
 
 void printVoid(void* v) {
-    //TODO printVoid
+    printHex((long)v);
 }
