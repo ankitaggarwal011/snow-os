616a6a4921b5de500e2caf7b3a499302fef2dc15
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2017-11-04 18:54:31 -0400

    Simplified paging code.

--- include/sys/paging.h
+++ include/sys/paging.h
@@ -1,5 +1,5 @@
 #include <sys/defs.h>
 
 void init_paging(uint64_t kernmem, uint64_t physbase, uint64_t physfree);
-void setup_page_tables(uint64_t virtual, uint64_t physical, uint64_t pages, uint64_t kernel_virtual_base);
+void setup_page_tables(uint64_t virtual, uint64_t physical);
 void _cr3();
--- sys/paging.c
+++ sys/paging.c
@@ -6,84 +6,30 @@
 #define PHYS_VIDEO_MEM 0xB8000
 #define ENTRIES 512
 
-uint64_t cr3_addr, *pml4_t;
+uint64_t cr3_addr, *pml4_t, kernel_virtual_base;
 
-void setup_page_tables(uint64_t virtual, uint64_t physical, uint64_t pages, uint64_t kernel_virtual_base) {
-    kprintf("Virtual: %x, Physical: %x, Pages: %x, KERNEL BASE: %x\n", virtual, physical, pages, kernel_virtual_base);
+void setup_page_tables(uint64_t virtual, uint64_t physical) {
     uint64_t *pdpe_t = NULL, *pde_t = NULL, *pte_t = NULL;
     uint32_t offset_pte = 0x1FF & (virtual >> 12),
              offset_pde = 0x1FF & (virtual >> 21),
              offset_pdpe = 0x1FF & (virtual >> 30),
              offset_pml4 = 0x1FF & (virtual >> 39);
-    if ((pml4_t[offset_pml4] & 1) == 1) { // it is present
-        pdpe_t = (uint64_t*) (kernel_virtual_base + pml4_t[offset_pml4]);
-        if ((pdpe_t[offset_pdpe] & 1) == 1) {
-            pde_t = (uint64_t*) (kernel_virtual_base + pdpe_t[offset_pdpe]);
-            if ((pde_t[offset_pde] & 1) == 1) {
-                pte_t = (uint64_t*) (kernel_virtual_base + pde_t[offset_pde]);
-            }
-            else {
-                uint64_t pte = get_free_page();
-                pde_t[offset_pde] = pte | 3;  
-                pte_t = (uint64_t*) (kernel_virtual_base + pte);
-            }
-        }
-        else {
-            uint64_t pde = get_free_page();
-            pdpe_t[offset_pdpe] = pde | 3;
-            pde_t = (uint64_t*) (kernel_virtual_base + pde);
-
-            uint64_t pte = get_free_page();
-            pde_t[offset_pde] = pte | 3;  
-            pte_t = (uint64_t*) (kernel_virtual_base + pte);
-        }
+    if ((pml4_t[offset_pml4] & 1UL) != 1UL) { // it is not present
+        pml4_t[offset_pml4] = get_free_page() | 3UL; // page is present and writable
+        kprintf("PDPE: %x\n", pml4_t[offset_pml4]);
     }
-    else {
-        uint64_t pdpe = get_free_page();
-        pml4_t[offset_pml4] = pdpe | 3; // page is present and writable
-        pdpe_t = (uint64_t*) (kernel_virtual_base + pdpe);
-
-        uint64_t pde = get_free_page();
-        pdpe_t[offset_pdpe] = pde | 3;
-        pde_t = (uint64_t*) (kernel_virtual_base + pde);
-
-        uint64_t pte = get_free_page();
-        pde_t[offset_pde] = pte | 3;  
-        pte_t = (uint64_t*) (kernel_virtual_base + pte);
+    pdpe_t = (uint64_t*) (kernel_virtual_base + pml4_t[offset_pml4]);
+    if ((pdpe_t[offset_pdpe] & 1UL) != 1UL) {
+        pdpe_t[offset_pdpe] = get_free_page() | 3UL;
+        kprintf("PDE: %x\n", pdpe_t[offset_pdpe]);
     }
-
-    uint32_t number_of_pte_t, j;
-    if (pages + offset_pte <= ENTRIES) {
-        for (uint64_t i = offset_pte; i < (offset_pte + pages); i++) {
-            pte_t[i] = physical | 3; 
-            physical += PAGE_SIZE;
-        }
+    pde_t = (uint64_t*) (kernel_virtual_base + pdpe_t[offset_pdpe]);
+    if ((pde_t[offset_pde] & 1UL) != 1UL) {
+        pde_t[offset_pde] = get_free_page() | 3UL;
+        kprintf("PTE: %x\n", pde_t[offset_pde]);
     }
-    else {
-        for (uint32_t i = offset_pte ; i < ENTRIES; i++) {
-            pte_t[i] = physical | 3;
-            physical += PAGE_SIZE;
-        }
-        pages = pages - (ENTRIES - offset_pte);
-        number_of_pte_t = pages / ENTRIES;
-        for (j = 1; j <= number_of_pte_t; j++) {
-            uint64_t pte = get_free_page();
-            pde_t[offset_pde + j] = pte | 3;
-            pte_t = (uint64_t*) (kernel_virtual_base + pte);
-            for(uint32_t k = 0; k < ENTRIES; k++ ) { 
-                pte_t[k] = physical | 3;
-                physical += PAGE_SIZE;
-            }
-        }
-        pages = pages - (ENTRIES * offset_pte);
-        uint64_t pte = get_free_page();
-        pde_t[offset_pde + j] = pte | 3;
-        pte_t = (uint64_t*) (kernel_virtual_base + pte);
-        for(uint32_t k = 0; k < pages; k++ ) { 
-            pte_t[k] = physical | 3;
-            physical += PAGE_SIZE;
-        }
-    }
+    pte_t = (uint64_t*) (kernel_virtual_base + pde_t[offset_pde]);
+    pte_t[offset_pte] = physical | 3UL;
 }
 
 void _cr3() {
@@ -95,12 +41,16 @@ void _cr3() {
 }
 
 void init_paging(uint64_t kernmem, uint64_t physbase, uint64_t physfree) {
-    uint64_t kernel_pages = (physfree - physbase) / PAGE_SIZE;
-    uint64_t kernel_virtual_base = kernmem - physbase;
+    uint64_t i = 0x0;
+    kernel_virtual_base = kernmem - physbase;
     cr3_addr = get_free_page();
     pml4_t = (uint64_t *) (kernel_virtual_base + cr3_addr);
-    pml4_t[510] = cr3_addr | 3;
-    setup_page_tables(kernmem, physbase, kernel_pages, kernel_virtual_base);
-    setup_page_tables(kernel_virtual_base + PHYS_VIDEO_MEM, PHYS_VIDEO_MEM, 1, kernel_virtual_base);
+    kprintf("PML4: %x\n", pml4_t);
+    pml4_t[510] = cr3_addr | 3UL;
+    while (physbase + i < physfree) {
+        setup_page_tables(kernmem + i, physbase + i);
+        i += PAGE_SIZE;
+    }
+    setup_page_tables(kernel_virtual_base + PHYS_VIDEO_MEM, PHYS_VIDEO_MEM);
     _cr3();
 }
