0144b26d97311411292ee34f39db540093d498c0
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2017-11-25 18:04:12 -0500

    elf

--- include/sys/string.h
+++ include/sys/string.h
@@ -4,7 +4,7 @@
 
 char* strcat(char *s1, const char *s2);
 int kstrcmp(char *s1, char *s2);
-int32_t oct_to_dec(int n);
+int32_t o_to_d(int n);
 int32_t atoi(char *p);
 uint64_t pow(int a, int b);
 
