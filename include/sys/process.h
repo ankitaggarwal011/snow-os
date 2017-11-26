5e709c9781177c02d17ead484afaccda3137e9a9
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2017-11-25 20:20:56 -0500

    elf

--- include/sys/process.h
+++ include/sys/process.h
@@ -1,9 +1,30 @@
 #include <sys/defs.h>
 
+#define PAGE_SIZE 4096
+#define HEAP_START 0x08000000
+#define STACK_START 0xF0000000
+
+#define CODE 1
+#define DATA 2
+#define HEAP 3
+#define STACK 4
+#define UNDEFINED 5
+
+#define PR 0x4
+#define PW 0x2
+#define PX 0x1
+
 struct vma_struct {
     uint64_t start;
     uint64_t end;
     uint64_t type;
     uint64_t flags;
     struct vma_struct *next;    
+};
+
+struct mm_struct {
+    struct vma_struct *vma_map;
+    uint64_t s_data, e_data;
+    uint64_t s_code, e_code;
+    uint64_t stack, heap;
 };
--- sys/elf64.c
+++ sys/elf64.c
@@ -12,18 +12,31 @@ void load_file(char *filename) {
     kprintf("Location of %s: %p\n", filename, location);
     kprintf("Reading ELF64 header:\n");
 
+    struct mm_struct *process = kmalloc(sizeof(struct mm_struct));
+
     Elf64_Ehdr *ehdr = (Elf64_Ehdr *) location;
     Elf64_Phdr *phdr = (Elf64_Phdr *)((uint64_t)ehdr + ehdr->e_phoff);
 
-    kprintf("RIP: %x\n", ehdr->e_entry);
+    kprintf("Task RIP: %x\n", ehdr->e_entry);
     int i = 0;
+
     struct vma_struct *vma_map = NULL;
     while (i < ehdr->e_phnum) {
         if (phdr->p_type == 1) {
             struct vma_struct *vma = (struct vma_struct*) kmalloc(sizeof(struct vma_struct));
             vma->start = phdr->p_vaddr;
             vma->end = phdr->p_vaddr + phdr->p_memsz;
+
+            uint64_t pages = (((vma->end / PAGE_SIZE + 1) * PAGE_SIZE) - ((vma->start / PAGE_SIZE) * PAGE_SIZE)) / PAGE_SIZE;
+            uint64_t v_addr = (phdr->p_vaddr / PAGE_SIZE) * PAGE_SIZE;
+            while(pages--) {
+                uint64_t page = get_free_page();
+                update_page_tables(v_addr, page, 0x7);
+                v_addr += PAGE_SIZE; 
+            }
+
             vma->flags = phdr->p_flags;
+            vma->type = UNDEFINED;
             vma->next = NULL;
             if (vma_map == NULL) {
                 vma_map = vma;
@@ -35,7 +48,25 @@ void load_file(char *filename) {
         phdr++;
         i++;
     }
-    struct vma_struct *test;
+    struct vma_struct *vma_heap = (struct vma_struct*) kmalloc(sizeof(struct vma_struct));
+    vma_heap->start = HEAP_START;
+    vma_heap->end = HEAP_START + PAGE_SIZE;
+    vma_heap->type = HEAP;
+    vma_heap->flags = PR | PW;
+    if (vma_map == NULL) {
+        vma_map = vma_heap;
+    }
+    else {
+        vma_map->next = vma_heap;
+    }
+    struct vma_struct *vma_stack = (struct vma_struct*) kmalloc(sizeof(struct vma_struct));
+    vma_stack->start = STACK_START + PAGE_SIZE;
+    vma_stack->end = STACK_START;
+    vma_stack->type = STACK;
+    vma_stack->flags = PR | PW;
+    vma_map->next = vma_stack;
+    process->vma_map = vma_map;
+    struct vma_struct *test = process->vma_map;
     kprintf("VMAs found: \n");
     while (test) {
         kprintf("VMA start: %x, VMA end: %x\n", test->start, test->end);
