951fcbeb7897d32d131cc9595002839b054f2286
Saraj Munjal <saraj.munjal@gmail.com>
2017-11-25 21:26:27 -0500

    Moved constants to header files, minor refactor

--- sys/paging.c
+++ sys/paging.c
@@ -1,24 +1,22 @@
 #include <sys/defs.h>
 #include <sys/kprintf.h>
 #include <sys/physical_memory.h>
+#include <sys/paging.h>
 
 #define PAGE_SIZE 4096
 #define PHYS_VIDEO_MEM 0xB8000
 #define ENTRIES 512
 
-#define KERNEL_R_W_FLAGS 0x7
-#define USER_R_W_FLAGS 0x7
-
 uint64_t *pml4_t, kernel_virtual_base;
 
-void update_page_tables(uint64_t virtual, uint64_t physical, uint16_t flags) {
+void update_page_tables(uint64_t virt_addr, uint64_t physical_addr, uint16_t flags) {
     uint64_t *pdpe_t = NULL, *pde_t = NULL, *pte_t = NULL;
     uint64_t pdpe, pde, pte;
     uint32_t
-    offset_pte = 0x1FF & (virtual >> 12),
-    offset_pde = 0x1FF & (virtual >> 21),
-    offset_pdpe = 0x1FF & (virtual >> 30),
-    offset_pml4 = 0x1FF & (virtual >> 39);
+    offset_pte = 0x1FF & (virt_addr >> 12),
+    offset_pde = 0x1FF & (virt_addr >> 21),
+    offset_pdpe = 0x1FF & (virt_addr >> 30),
+    offset_pml4 = 0x1FF & (virt_addr >> 39);
 
     if ((pml4_t[offset_pml4] & 1UL) != 1UL) {
         pdpe = get_free_page();
@@ -43,7 +41,7 @@ void update_page_tables(uint64_t virtual, uint64_t physical, uint16_t flags) {
     }
     pde_t[offset_pde] = pte | flags;
     pte_t = (uint64_t * )(kernel_virtual_base + pte);
-    pte_t[offset_pte] = physical | flags;
+    pte_t[offset_pte] = physical_addr | flags;
 }
 
 void set_new_cr3(uint64_t cr3_addr) {
@@ -57,11 +55,11 @@ void init_paging(uint64_t kernmem, uint64_t physbase, uint64_t physfree) {
     // map all physical memory to virtual memory (identity mapping)
     // better choice for 64 bit systems (used by Linux)
     while (p_i < (physfree + get_free_pages_count() * PAGE_SIZE)) {
-        update_page_tables(v_i, p_i, KERNEL_R_W_FLAGS);
+        update_page_tables(v_i, p_i, PAGING_KERNEL_R_W_FLAGS);
         v_i += PAGE_SIZE;
         p_i += PAGE_SIZE;
     }
-    update_page_tables(kernel_virtual_base + PHYS_VIDEO_MEM, PHYS_VIDEO_MEM, KERNEL_R_W_FLAGS);
+    update_page_tables(kernel_virtual_base + PHYS_VIDEO_MEM, PHYS_VIDEO_MEM, PAGING_KERNEL_R_W_FLAGS);
     set_new_cr3(cr3_addr);
     /* this is causing bootloop, can be debugged later, not important at this stage
     // map physical memory from 0x0 to physbase
@@ -95,7 +93,7 @@ void *kmalloc(uint32_t bytes_required) {
         // give contiguous virtual space
         // this might lead to overwriting virtual address used by other process
         // check page bits to avoid that later on
-        update_page_tables(kernel_virtual_base + allocated_addr + i * PAGE_SIZE, ph_addr, KERNEL_R_W_FLAGS);
+        update_page_tables(kernel_virtual_base + allocated_addr + i * PAGE_SIZE, ph_addr, PAGING_KERNEL_R_W_FLAGS);
     }
     return (void *) (kernel_virtual_base + allocated_addr);
 }
