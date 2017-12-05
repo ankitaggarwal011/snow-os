96cbd949e11581b4618a06a130e57ee665ee1174
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2017-12-04 22:29:57 -0500

    Testing kfree

--- libc/free.c
+++ libc/free.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 void free(void *ptr) {
     return syscall2(SYSCALL_FREE, ptr);
--- libc/malloc.c
+++ libc/malloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 void *malloc(size_t size) {
     return syscall2(SYSCALL_MALLOC, size);
