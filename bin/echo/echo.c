8c3da402ff7c8a01b839439835d781867be6ae52
Saraj Munjal <saraj.munjal@gmail.com>
2017-12-07 13:28:59 -0500

    Minor fix in echo

--- bin/echo/echo.c
+++ bin/echo/echo.c
@@ -4,10 +4,11 @@
 
 int main(int argc, char *argv[], char *envp[]) {
     if (argc < 2) {
+        while (1);
         return 0;
     }
     for (int i = 1; i < argc; i++) {
-        char *s = argv[i - 1];
+        char *s = argv[i];
         write(1, s, strlen(s));
         write(1, " ", 1);
     }
